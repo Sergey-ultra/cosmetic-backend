2a6e22e70775d2818fe5e47f81ee4eaf0b731f02
Sergey-ultra <maasa@list.ru>
2023-03-25 18:33:21 +0300

    frontend add buttonComponent

--- resources/js/Admin/components/button-component.vue
+++ resources/js/Admin/components/button-component.vue
@@ -4,8 +4,11 @@
         class="button"
         :disabled="disabled"
         :class="{
-            [`${color}`]: Boolean(color),
-            [`${size}`]: Boolean(size),
+            [`button--${color}`]: Boolean(color),
+            [`button--${size}`]: Boolean(size),
+            'button--icon': icon,
+            'button--outline': outline,
+            'button--round': round,
         }"
     >
         <slot></slot>
@@ -34,7 +37,29 @@ export default {
         },
         color: {
             type: String,
-        }
+        },
+        /**
+         * Стиль иконка
+         */
+        icon: {
+            type: Boolean,
+            default: false,
+        },
+        /**
+         * Стиль с обводкой
+         */
+        outline: {
+            type: Boolean,
+            default: false,
+        },
+        /**
+         * Стиль скругленный
+         */
+        round: {
+            type: Boolean,
+            default: false,
+        },
+
     },
     computed: {
 
@@ -55,7 +80,10 @@ export default {
         border-radius: 4px;
         color: #fff;
         background: rgb(24, 103, 192) none repeat scroll 0% 0%;
-        border: 0;
+        border: 0 solid #616469 !important;
+        a {
+            color: inherit;
+        }
         &:before {
             background-color: currentColor;
             border-radius: inherit;
@@ -78,19 +106,93 @@ export default {
             background-color: rgba(0,0,0,.12);
             color: rgba(0,0,0,.26);
         }
+        &--icon {
+            padding-left: 0;
+            padding-right: 0;
+            text-align: center;
+        }
+        &.button--icon {
+            padding-left: 0;
+            padding-right: 0;
+            width: 32px !important;
+        }
+        &.button--round {
+            border-radius: 50%;
+        }
+        &.button--outline {
+            background: hsla(220,4%,50%,.05) !important;
+            border-color: #7a7d83 !important;
+            box-shadow: none;
+            color: #7a7d83 !important;
+            line-height: 30px;
+            text-shadow: none !important;
+            border-width: 1px !important;
+        }
+        &--small {
+            min-width: 28px;
+            height: 28px;
+            padding: 0 12.4px;
+        }
+        &--yellow {
+            background-color: #fc0;
+            color: #000000;
+        }
+        &--grey {
+            background: #e0e0e0 !important;
+            border-color: #7e7c7c !important;
+            box-shadow: 0 0 2px rgba(0,0,0,.05),0 1px 1px rgba(0,0,0,.1);
+            color: #fff !important;
+            text-shadow: none !important;
+        }
+        &--blue {
+            background-color: rgb(0, 91, 255);
+        }
+        &--green {
+            background: #3fa149 !important;
+            border-color: #317c38 !important;
+            box-shadow: 0 0 2px rgba(0,0,0,.05),0 1px 1px rgba(0,0,0,.1);
+            color: #fff !important;
+            text-shadow: none !important;
+
+            &.button--outline {
+                background: rgba(63,161,73,.05) !important;
+                border-color: #3fa149 !important;
+                box-shadow: none;
+                color: #3fa149 !important;
+                text-shadow: none !important;
+                &:hover {
+                    background: #4bb956 !important;
+                    border-color: #4bb956 !important;
+                    box-shadow: none;
+                    color: #fff !important;
+                    text-shadow: none !important;
+                }
+            }
+        }
+        &--red {
+            background: #d8443a !important;
+            border-color: #ba2e25 !important;
+            box-shadow: 0 0 2px rgba(0,0,0,.05),0 1px 1px rgba(0,0,0,.1);
+            color: #fff !important;
+            text-shadow: none !important;
+            &.button--outline {
+                background: rgba(216, 68, 58, .05) !important;
+                border-color: #d8443a !important;
+                box-shadow: none;
+                color: #d8443a !important;
+                text-shadow: none !important;
+
+                &:hover {
+                    background: #de6058 !important;
+                    border-color: #de6058 !important;
+                    box-shadow: none;
+                    color: #fff !important;
+                    text-shadow: none !important;
+                }
+            }
+        }
     }
-    .small {
-        min-width: 28px;
-        height: 28px;
-        padding: 0 12.4px;
-    }
-    .yellow {
-        background-color: #fc0;
-        color: #000000;
-    }
-    .blue {
-        background-color: rgb(0, 91, 255);
-    }
+
 
     .loader {
         position: absolute;
