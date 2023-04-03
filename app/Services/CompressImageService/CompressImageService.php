683afcef2487a3e98e43229705fa5aa1375eaa19
Sergey-ultra <maasa@list.ru>
2023-04-03 20:54:29 +0300

    change CompressImageService.php

--- app/Services/CompressImageService/CompressImageService.php
+++ app/Services/CompressImageService/CompressImageService.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Services\CompressImageService;
 
 
+use Illuminate\Support\Facades\Log;
 use Imagick;
 use ImagickException;
 
@@ -27,6 +28,7 @@ class CompressImageService implements CompressImageInterface
 
             return ['status' => true];
         } catch (ImagickException $e) {
+            Log::error((string)$e);
             return ['status' => false, 'message' => $e->getMessage()];
         }
 
