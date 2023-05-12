1d6ee4a9b55acffc1fb5c3bc5bdbea3f852bd5dd
Sergey-ultra <maasa@list.ru>
2023-05-12 18:31:58 +0300

    add updateTelegramUser method to UserController.php

--- config/telegrambot.php
+++ config/telegrambot.php
@@ -3,7 +3,7 @@ return [
     'admin_ip' => env('ADMIN_IP_ADDRESS'),
     'admin_notification_api_url' => 'https://api.tlgr.org/bot' . env('TELEGRAM_ADMIN_NOTIFICATION_API_KEY'),
     'admin_notification_url' => env('TELEGRAM_ADMIN_NOTIFICATION_URL'),
-    'user_notification_api_url' => 'https://api.tlgr.org/bot' . env('TELEGRAM_USER_NOTIFICATION_API_KEY'),
+    'user_notification_api_url' => 'https://api.tlgr.org/bot' . env('TELEGRAM_USER_NOTIFICATION_API_KEY') . '/',
     'user_notification_url' => env('TELEGRAM_USER_NOTIFICATION_URL'),
     'admin_user_id' => env('TELEGRAM_ADMIN_USER_ID'),
 ];
