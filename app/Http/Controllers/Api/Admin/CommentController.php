77344ec62c358b710adeb4d1bd3b5001ad1c6f71
Sergey-ultra <maasa@list.ru>
2023-06-10 13:33:45 +0300

    fix method index in Admin\CommentController

--- app/Http/Controllers/Api/Admin/CommentController.php
+++ app/Http/Controllers/Api/Admin/CommentController.php
@@ -9,6 +9,10 @@ namespace App\Http\Controllers\Api\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\Traits\DataProvider;
 use App\Models\Comment;
+use App\Models\Product;
+use App\Models\Review;
+use App\Models\Sku;
+use App\Models\SkuRating;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
@@ -25,18 +29,44 @@ class CommentController extends Controller
      */
     public function index(Request $request): JsonResponse
     {
-        $perPage = (int)  ($request->per_page ?? 10);
+        $perPage = (int)($request->per_page ?? 10);
 
         $query = Comment::query()
             ->select([
                 sprintf('%s.id', Comment::TABLE),
-                sprintf('%s.review_id', Comment::TABLE),
                 sprintf('%s.comment', Comment::TABLE),
                 sprintf('%s.reply_id', Comment::TABLE),
                 sprintf('%s.status', Comment::TABLE),
                 sprintf('%s.created_at', Comment::TABLE),
+                sprintf('%s.review_id', Comment::TABLE),
+                sprintf('%s.name', Product::TABLE),
+                sprintf('%s.volume', Sku::TABLE),
                 sprintf('%s.name AS user_name', User::TABLE),
             ])
+            ->join(
+                Review::TABLE,
+                sprintf('%s.review_id', Comment::TABLE),
+                '=',
+                sprintf('%s.id', Review::TABLE)
+            )
+            ->join(
+                SkuRating::TABLE,
+                sprintf('%s.sku_rating_id', Review::TABLE),
+                '=',
+                sprintf('%s.id', SkuRating::TABLE)
+            )
+            ->join(
+                Sku::TABLE,
+                sprintf('%s.sku_id', SkuRating::TABLE),
+                '=',
+                sprintf('%s.id', Sku::TABLE)
+            )
+            ->join(
+                Product::TABLE,
+                sprintf('%s.product_id', Sku::TABLE),
+                '=',
+                sprintf('%s.id', Product::TABLE)
+            )
             ->leftJoin(
                 User::TABLE,
                 sprintf('%s.user_id', Comment::TABLE),
