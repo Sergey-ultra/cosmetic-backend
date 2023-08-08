29ec68f701a9840af76fb3dc17a111068f0028fc
Sergey-ultra <maasa@list.ru>
2023-08-08 11:36:05 +0300

    edit Socialite Authorization

--- app/Http/Controllers/Api/AuthSocialController.php
+++ app/Http/Controllers/Api/AuthSocialController.php
@@ -16,7 +16,7 @@ use Laravel\Socialite\Facades\Socialite;
 
 class AuthSocialController extends Controller
 {
-    public const AVAILABLE_SERVICES = ['google', 'vkontakte'];
+    public const AVAILABLE_SERVICES = ['google', 'vkontakte', 'facebook', 'instagram', 'mailru'];
 
     /**
      * @param string $service
