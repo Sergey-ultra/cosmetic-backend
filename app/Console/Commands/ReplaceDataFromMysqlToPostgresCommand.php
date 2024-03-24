85626d2897c7fe2371def9bd00333b6ff643da20
Sergey-ultra <maasa@list.ru>
2024-03-24 03:58:31 +0300

    fix command

--- app/Console/Commands/ReplaceDataFromMysqlToPostgresCommand.php
+++ app/Console/Commands/ReplaceDataFromMysqlToPostgresCommand.php
@@ -50,8 +50,8 @@ WHERE table_schema = 'sanctum';");
             ->map(fn (stdClass $item) => get_object_vars($item))
             ->all();
 
-        if (count($rows) > 5000) {
-            $rows = array_chunk($rows, 5000, true);
+        if (count($rows) > 1500) {
+            $rows = array_chunk($rows, 1500, true);
             echo 'Использование памяти ' . (memory_get_usage(true) / 1024 / 1024) . \PHP_EOL;
             foreach ($rows as $chunk) {
                 DB::table($table)->insert($chunk);
